22ff74a6d901a887389fd2b337ebb4ff8db8d13a
hiromitsu <hitsk26@gmail.com>
2013-07-18 16:39:47 +0900

    implemented Wheel motor

--- Strange_Lovers/WheelMotor.c
+++ Strange_Lovers/WheelMotor.c
@@ -1,12 +1,18 @@
 #include "WheelMotor.h"
 
 
+
+void WheelMotor_init(WheelMotor *this_WheelMotor,U32 port_id){
+	this_WheelMotor->port_id = port_id;
+}
+
 int WheelMotor_get_count(WheelMotor *this_WheelMotor)
 {
-	return 0;
+	return Motor_get_count(this_WheelMotor->port_id);
 }
 
 void WheelMotor_drive_motor(WheelMotor *this_WheelMotor, int pwm)
 {
+Motor_set_speed(this_WheelMotor->port_id,pwm,1); //3rd paramter means barake mode
 
 }
