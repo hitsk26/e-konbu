5a2c4d4a40d7f9a64cd75ac7255a9c5908c79368
hiromitsu <hitsk26@gmail.com>
2013-07-18 13:44:26 +0900

    to use arrow operator instated of . operator on calibration method

--- Strange_Lovers/Calibration.c
+++ Strange_Lovers/Calibration.c
@@ -10,8 +10,6 @@ void Calibration_init(Calibration *this_Calibration){
 
 void Calibration_calibration(Calibration *this_Calibration){
 
-	static int flg = 0;
-
 	static U32	avg_cnt = 0;
 	static U32	cal_start_time;	
 
@@ -26,11 +24,11 @@ void Calibration_calibration(Calibration *this_Calibration){
 
 	while((ecrobot_get_systick_ms() - cal_start_time) < 1000U){
 		/* ジャイロセンサの設定をする */
-		Calibration.gyro += (U32)ecrobot_get_gyro_sensor(NXT_PORT_S1);
+		this_Calibration->gyro += (U32)ecrobot_get_gyro_sensor(NXT_PORT_S1);
 		avg_cnt++;
 	}
 
-		Calibration.gyro /= avg_cnt;
+		this_Calibration->gyro /= avg_cnt;
 		ecrobot_sound_tone(440U, 500U, 30U);
 
 	systick_wait_ms(500);
@@ -39,7 +37,7 @@ void Calibration_calibration(Calibration *this_Calibration){
 	while(1){
 		if(ecrobot_get_touch_sensor(NXT_PORT_S4) == TRUE){
 			ecrobot_sound_tone(906, 512, 30);
-			Calibration.black_value = ecrobot_get_light_sensor(NXT_PORT_S3);
+			this_Calibration->black_value = ecrobot_get_light_sensor(NXT_PORT_S3);
 			systick_wait_ms(500);
 			break;
 		}
@@ -49,15 +47,15 @@ void Calibration_calibration(Calibration *this_Calibration){
 	while(1){
 		if(ecrobot_get_touch_sensor(NXT_PORT_S4) == TRUE){
 			ecrobot_sound_tone(906, 512, 30);
-			Calibration.white_value = ecrobot_get_light_sensor(NXT_PORT_S3);
+			this_Calibration->white_value = ecrobot_get_light_sensor(NXT_PORT_S3);
 			systick_wait_ms(500);
 			break;
 		}
 	}
 
-	Balancer_set_gyro(&balancer , Calibration.gyro);
+	Balancer_set_gyro(&balancer ,this_Calibration->gyro);
 
-	Target_value_set_target_value_brightness(&target_value,(Calibration.black_value+Calibration.white_value)/2);
+	Target_value_set_target_value_brightness(&target_value,(this_Calibration->black_value+this_Calibration->white_value)/2);
 
 
 	print_calibrate_value();
