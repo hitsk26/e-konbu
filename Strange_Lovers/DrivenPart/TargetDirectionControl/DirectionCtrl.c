b17a1e916aeb2db3eec8b94c0d1beeb5899cf77e
hiromitsu <hitsk26@gmail.com>
2013-07-21 15:40:41 +0900

    added DirectioCtrl Class skelton

---
+++ Strange_Lovers/DrivenPart/TargetDirectionControl/DirectionCtrl.c
@@ -0,0 +1,13 @@
+#include "DirectionCtrl.h"
+#include "../../Factory.h"
+
+
+int DirectionControl_run(DirectionCtrl *this_DirectionCtrl)
+{
+	return 0;
+}
+
+int DirectionCtrl_do_direction_ctrl(DirectionCtrl *this_DirectionCtrl)
+{
+	return 0;
+}
---
+++ Strange_Lovers/DrivenPart/TargetDirectionControl/DirectionCtrl.h
@@ -0,0 +1,14 @@
+#ifndef DRIVENPART_TARGETDIRECTIONCONTROL_DIRECTION_CTRL_H
+#define DRIVENPART_TARGETDIRECTIONCONTROL_DIRECTION_CTRL_H
+
+
+typedef struct {
+	
+}DirectionCtrl;
+
+
+int DirectionCtrl_do_direction_ctrl(DirectionCtrl *this_DirectionCtrl);
+int DirectionControl_run(DirectionCtrl *this_DirectionCtrl);
+
+
+#endif
