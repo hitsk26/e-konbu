23960c604eab275071f44dde069d3c2776b04a48
GuRaPamyu <jp7fkf@gmail.com>
2013-08-07 15:16:55 +0900

    add speed and timer switch function

--- Strange_Lovers/StrategyPart/RunningMethodSwitchJudge/SpeedTimerMultipleSwitch.c
+++ Strange_Lovers/StrategyPart/RunningMethodSwitchJudge/SpeedTimerMultipleSwitch.c
@@ -5,12 +5,15 @@ int SpeedTimerMultipleSwitch_judge_switch_method(SwitchTerm switch_term){
 	
 	int speed =  SpeedEncoder_get_speed(&speedEncoder);
 	int time = Timer_get_ms(&timer);
+	int judge_flag=0;
 
-	/*
 	if(switch_term.speed <= speed && switch_term.time >= time){
+		judge_flag = 1;
+		Timer_reset(&timer);
+	}
+	else{
+		judge_flag = 0;
+	}
 
-	}
-	else{ 
-	}
-	*/
+	return judge_flag;
 }
