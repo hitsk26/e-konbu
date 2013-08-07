407e7c4aca7060a26a756da837e3a3d68af7a019
GuRaPamyu <jp7fkf@gmail.com>
2013-08-07 14:30:12 +0900

    1

---
+++ Strange_Lovers/StrategyPart/RunningMethodSwitchJudge/SpeedTimerMultipleSwitch.c
@@ -0,0 +1,16 @@
+#include "SpeedTimerMultipleSwitch.h"
+#include "../../Factory.h"
+
+int SpeedTimerMultipleSwitch_judge_switch_method(SwitchTerm switch_term){
+	
+	int speed =  SpeedEncoder_get_speed(&speedEncoder);
+	int time = Timer_get_ms(&timer);
+
+	/*
+	if(switch_term.speed <= speed && switch_term.time >= time){
+
+	}
+	else{ 
+	}
+	*/
+}
