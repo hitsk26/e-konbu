540510a8e12ec7a85a3e60807f6defdc06aa69ea
hiromitsu <hitsk26@gmail.com>
2013-08-21 01:10:57 +0900

    update DistanceSwitch

--- Strange_Lovers/StrategyPart/RunningMethodSwitchJudge/DistanceSwitch.c
+++ Strange_Lovers/StrategyPart/RunningMethodSwitchJudge/DistanceSwitch.c
@@ -1,18 +1,27 @@
 #include "DistanceSwitch.h"
 #include "../../Factory.h"
 
-
 int DistanceSwitch_judge_switch_method(SwitchTerm switch_term)
 {
 	int judge_flag=0;
 
-	if(DistanceEncoder_get_distance(&distanceEncoder) > switch_term.distance){
-			judge_flag=1;
-			DistanceEncoder_reset_Encoder(&distanceEncoder);
-	}
-	else {
-		judge_flag=0;
+	if(switch_term.distance >= 0)
+	{
+		if(DistanceEncoder_get_distance(&distanceEncoder) > switch_term.distance){
+				judge_flag=1;
+				DistanceEncoder_reset_Encoder(&distanceEncoder);
+		}
+		else {
+			judge_flag=0;
+		}
+	}else{
+		if(DistanceEncoder_get_distance(&distanceEncoder) < switch_term.distance){
+				judge_flag=1;
+				DistanceEncoder_reset_Encoder(&distanceEncoder);
+		}
+		else {
+			judge_flag=0;
+		}
 	}
 	return judge_flag;
 }
-
