50abe5ccfdd9153a4840def250c4f14e358fcf69
hiromitsu <hitsk26@gmail.com>
2013-09-19 15:05:28 +0900

    modified mymath and stack size on prog.oil because of ram size limitation

--- Strange_Lovers/lib/mymath.c
+++ Strange_Lovers/lib/mymath.c
@@ -64,5 +64,69 @@ return s;
 
 double mycos(double x)
 {
-	return mysin(PI / 2 - x);
+	return my_sin(PI / 2 - x);
+}
+
+
+
+double my_sin(double x)
+{
+  double result1, result2, result;
+  int n;
+
+  result1 = 0;
+  result2 = 0;
+
+  //ループを回す回数(ここでは5)を大きくすると精度が上がります
+  for(n = 1;n <= 3;n++)
+    {
+      result1 += my_pow(x, 4 * n - 3) / fact(4 * n - 3);
+    }
+
+
+  //ループを回す回数(ここでは5)を大きくすると精度が上がります
+  for(n = 1; n <= 3;n++)
+    {
+      result2 += my_pow(x, 4 * n - 1) / fact(4 * n - 1);
+    }
+
+  result =  result1 - result2;
+
+  return result;
+}
+
+//xのn乗を計算する関数
+double my_pow(double x, int n)
+{
+  int i;
+  double pow_result = 1;
+
+  if(n == 0)
+    return 1;
+  else
+    {
+      for(i = 0;i < n;i++)
+	{
+	  pow_result *= x;
+	}
+      return pow_result;
+    }
+}
+
+//xの階乗を計算する関数
+
+int fact(int n)
+{
+  int i, result = 1;
+
+  if(n == 0)
+    return 1;
+  else
+    {
+      for(i = 1;i <= n;i++)
+	{
+	  result *= i;
+	}
+      return result;
+    }
 }
--- Strange_Lovers/lib/mymath.h
+++ Strange_Lovers/lib/mymath.h
@@ -9,4 +9,14 @@ extern float rad2deg(float radian);
 extern float abs(float value);
 extern double mysin(double x);
 extern double mycos(double x);
+
+
+double my_sin(double x);
+
+//x‚Ìnæ‚ğŒvZ‚·‚éŠÖ”
+double my_pow(double x, int n);
+
+//x‚ÌŠKæ‚ğŒvZ‚·‚éŠÖ”
+
+int fact(int n);
 #endif
