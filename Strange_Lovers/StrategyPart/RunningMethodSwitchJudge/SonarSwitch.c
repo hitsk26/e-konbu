67e5c49c9a30e349bb5e01e51af39bee652d7126
hiromitsu <hitsk26@gmail.com>
2013-09-20 21:28:36 +0900

    switch term initialize modified

--- Strange_Lovers/StrategyPart/RunningMethodSwitchJudge/SonarSwitch.c
+++ Strange_Lovers/StrategyPart/RunningMethodSwitchJudge/SonarSwitch.c
@@ -4,11 +4,12 @@
 int SonarSwitch_judge_switch_method(SwitchTerm switch_term)
 {
 	int judge_flag=0;
-	float distance_obstacle = SonarEncoder_get_distance_obstacle(&sonorEncoder);
+	int distance_obstacle = SonarEncoder_get_distance_obstacle(&sonorEncoder);
 	
 	if(switch_term.distance_obstacle >= distance_obstacle){
 		judge_flag=1;
 	}
+	ecrobot_debug1(switch_term.distance_obstacle,distance_obstacle,switch_term.distance);
 	return judge_flag;
 }
 
