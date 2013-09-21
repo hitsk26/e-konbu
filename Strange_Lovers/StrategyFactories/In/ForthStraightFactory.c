f3fa0d0b15918796c19ee91e29e04e506850c89d
yuukikaito <s39340@sendai-nct.jp>
2013-09-22 00:13:27 +0900

    Incourse fourthcourve fixed

--- Strange_Lovers/StrategyFactories/In/ForthStraightFactory.c
+++ Strange_Lovers/StrategyFactories/In/ForthStraightFactory.c
@@ -22,10 +22,10 @@ void forth_straight_factory_init()
 	
 
 	//forthStraightLineTraceMethod
-	use_controller.target_curvature_controller_weight = 0.0;
-	use_controller.target_light_controller_weight = 1.0;
+	use_controller.target_curvature_controller_weight = 0.5;
+	use_controller.target_light_controller_weight = 0.5;
 	//switch_term.distance = 1600;
-	switch_term.distance = 870000;
+	switch_term.distance = 870;
 	balancing_requrement = 1;
 	target_tail_angle=0;
 	gyroOffsetRevise=0;
