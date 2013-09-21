e74f44fd07802a01eb6742b50c714889bfb3903a
hiromitsu <hitsk26@gmail.com>
2013-09-22 04:36:22 +0900

    weight change on SecondCurve for Out

--- Strange_Lovers/StrategyFactories/Out/SecondCurveFactory.c
+++ Strange_Lovers/StrategyFactories/Out/SecondCurveFactory.c
@@ -18,8 +18,8 @@ void second_curve_factory_init(){
 	MovementDirection movementDirection = FORWARD;
 	
 	//secondCurve_1_LineTraceRunningMethod
-	use_controller.target_curvature_controller_weight = 0.3;
-	use_controller.target_light_controller_weight = 0.7;
+	use_controller.target_curvature_controller_weight = 0.4;
+	use_controller.target_light_controller_weight = 0.6;
 	switch_term.distance = 1141.592;
 	switch_term.inclination = 0;
 	switch_term.inPushed = 0;
@@ -47,8 +47,8 @@ void second_curve_factory_init(){
 
 	//secondCurve_2_LineTraceRunningMethod
 	
-	use_controller.target_curvature_controller_weight = 0.5;
-	use_controller.target_light_controller_weight = 0.5;
+	use_controller.target_curvature_controller_weight = 0.4;
+	use_controller.target_light_controller_weight = 0.6;
 	switch_term.distance = 643;
 	switch_term.inclination = 0;
 	switch_term.inPushed = 0;
@@ -75,8 +75,8 @@ void second_curve_factory_init(){
 
 
 	//secondCurve_3_LineTraceRunningMethod
-	use_controller.target_curvature_controller_weight = 0.5;
-	use_controller.target_light_controller_weight = 0.5;
+	use_controller.target_curvature_controller_weight = 0.4;
+	use_controller.target_light_controller_weight = 0.6;
 	//switch_term.distance = 561.34;
 	switch_term.distance = 600.34;
 
