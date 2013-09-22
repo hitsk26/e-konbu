dfd09ce9bba6d12548d86a1a9997e2086b2fe45e
yuukikaito <s39340@sendai-nct.jp>
2013-09-22 17:29:31 +0900

    速度制御調整

--- Strange_Lovers/DrivenPart/TargetSpeedControl/PIDSpeedCtrlParm.c
+++ Strange_Lovers/DrivenPart/TargetSpeedControl/PIDSpeedCtrlParm.c
@@ -1,8 +1,9 @@
 #include "PIDSpeedCtrlParm.h"
 
 void PSCP_init(PIDSpeedCtrlParm *this_PIDSpeedCtrlParm){
-	this_PIDSpeedCtrlParm->sKp = 0.0008;
-	//this_PIDSpeedCtrlParm->sKd = 0.00001;
+	this_PIDSpeedCtrlParm->sKp = 0.0006;
+	this_PIDSpeedCtrlParm->sKd = 0.0000008;
+	this_PIDSpeedCtrlParm->sKi = 0.0000004;
 }
 
 float PSCP_getSKp(PIDSpeedCtrlParm *this_PIDSpeedCtrlParm){
